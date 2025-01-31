eb86e71163a75c96b33270d703dcb2d563320f5a
999usif <yousefalmallah@yahoo.com>
2025-01-31 08:15:03 +0100

    Added trouble (pretty UI for diagnostics)

--- lua/config/vim-binds.lua
+++ lua/config/vim-binds.lua
@@ -3,20 +3,28 @@ vim.cmd("set number")
 
 -- Set tab = 4 spaces
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 
--- Insert mode mapping: Ctrl+E moves to the end of the line
-vim.api.nvim_set_keymap('i', '<C-e>', '<Esc>A', { noremap = true, silent = true })
+return {
+  config = function()
+    -- Insert mode mapping: Ctrl+E moves to the end of the line
+    vim.api.nvim_set_keymap('i', '<C-e>', '<Esc>A', { noremap = true, silent = true })
 
--- Normal mode: Map `¤` to `$`
-vim.api.nvim_set_keymap('n', '¤', '$', { noremap = true, silent = true })
+    -- Normal mode: Map `¤` to `$`
+    vim.api.nvim_set_keymap('n', '¤', '$', { noremap = true, silent = true })
 
--- Visual mode: Map `¤` to `$`
-vim.api.nvim_set_keymap('x', '¤', '$', { noremap = true, silent = true })
+    -- Visual mode: Map `¤` to `$`
+    vim.api.nvim_set_keymap('x', '¤', '$', { noremap = true, silent = true })
 
--- Operator-pending mode: Map `¤` to `$`
-vim.api.nvim_set_keymap('o', '¤', '$', { noremap = true, silent = true })
+    -- Operator-pending mode: Map `¤` to `$`
+    vim.api.nvim_set_keymap('o', '¤', '$', { noremap = true, silent = true })
 
+    -- Mapping to add newline and go back to normal mode
+    vim.keymap.set('n', '<leader>o', 'o<Esc>k', { noremap = true, silent = true })
 
+    -- Mapping to add newline and go back to normal mode
+    vim.api.nvim_set_keymap('n', '<leader>O', 'O<Esc>j', { noremap = true, silent = true })
+  end,
+}
