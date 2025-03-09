35024635ad3a967d790d7fb8f0ee99428154421b
999usif <151412279+999usif@users.noreply.github.com>
2025-03-09 12:46:38 +0100

    Fixed luasnip <S-Tab> and <Tab> errors with luasnip

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -44,8 +44,8 @@ return {
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
+            elseif require("luasnip").expand_or_jumpable() then
+              require("luasnip").expand_or_jump()
             else
               fallback()
             end
@@ -54,8 +54,8 @@ return {
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
+            elseif require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1)
             else
               fallback()
             end
